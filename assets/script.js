8a75ee301cbb123042f3ef7d0288d1ac38cfb7d6
Vincent Vo <itsvnvo@gmail.com>
2021-03-13 12:19:02 -0600

    Added color to row based on time, storage currently inop

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ var timeBlockEl = [
     { time: "4 PM " },
     { time: "5 PM " }
 ];
-
+var timeEl = moment().format('h A');
 
 // Displays current day and time
 function displayTime() {
@@ -26,26 +26,35 @@ setInterval(displayTime, 1000);
 
 // Makes rows and the columns including the textarea and button at the end in timedisplay
 timeBlockEl.forEach(function (timeRows) {
-    var timeDisplay = '<div class="row time-block col-1"></div>'
-    var getStorage = JSON.parse(localStorage.getItem("btnSave" + counter))
-    var textRow = '<textarea id=id' + counter + ' <div class="col-8 justify-content-center time-block" ></textarea></div>'
-    var btnEl = '<button id=btnSave' + counter + ' <div class="btn saveBtn col-1"><i class="fas fa-save"></i></button></div>'
+    var timeDisplay = '<div class="time-block">'
+    var textRow = '<div id="ppf' + counter + '" ><textarea class="row col-10" id="id' + counter + '"></textarea>'
+    var btnEl = '<button id=btnSave' + counter + ' ><div class="col-1 btn saveBtn"><i class="fas fa-save"></i></button></div></div></div>'
     timeDisplay += timeRows.time + textRow + btnEl
+    var getStorage = JSON.parse(localStorage.getItem("btnSave" + counter));
     $(".container").append(timeDisplay);
+    var parent = document.getElementById("ppf" + counter);
     if (getStorage) {
-        document.getElementById("id" + counter).value = getStorage
+        parent.querySelector("#" + "id" + counter).value = getStorage
     }
+    if (timeRows.time < timeEl) {
+        document.getElementById("ppf" + counter).className += "past";
+    } else if (timeRows.time == timeEl) {
+        document.getElementById("ppf" + counter).className += "present";
+    } else {
+        document.getElementById("ppf" + counter).className+= "future";
+    }
+    // console.log(localStorage)
     counter++
 });
 
-
+console.log($("div"))
 // Saves the local storage
 $(document).ready(function () {
     $("#btnSave0, #btnSave1, #btnSave2, #btnSave3, #btnSave4, #btnSave5, #btnSave6, #btnSave7, #btnSave8").on("click", function () {
-        var task = $(this).prev().val();
-        localStorage.setItem($(this).attr('id'), JSON.stringify(task));
+        var task = $("#ppf").children("textarea").val();
+        localStorage.setItem($(this).attr("#" + "id" + counter), JSON.stringify(task));
+        console.log($("#ppf").children("textarea"))
         console.log(task);
-
     }
     )
 });
