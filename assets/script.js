df3330953355f1f17d216e82061a8b289a75fcdc
Vincent Vo <itsvnvo@gmail.com>
2021-03-13 15:09:43 -0600

    Moved if statement outside into a forloop, added a block array variable

--- assets/script.js
+++ assets/script.js
@@ -2,59 +2,62 @@
 var timeDisplayEl = $('#time-display');
 var counter = 0
 var timeBlockEl = [
-    { time: "9 AM " },
+    { time: "9 AM" },
     { time: "10 AM" },
     { time: "11 AM" },
     { time: "12 PM" },
-    { time: "1 PM " },
-    { time: "2 PM " },
-    { time: "3 PM " },
-    { time: "4 PM " },
-    { time: "5 PM " }
+    { time: "1 PM" },
+    { time: "2 PM" },
+    { time: "3 PM" },
+    { time: "4 PM" },
+    { time: "5 PM" }
 ];
-var timeEl = moment().format('h A');
+var blockArray = document.getElementsByTagName("textarea");
+var hourNow = moment().format('h A');
 
 // Displays current day and time
 function displayTime() {
-    var timeNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplayEl.text(timeNow);
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
 }
 
-// Makes current day time tick by 1 second
+
 setInterval(displayTime, 1000);
 
-
-// Makes rows and the columns including the textarea and button at the end in timedisplay
 timeBlockEl.forEach(function (timeRows) {
-    var timeDisplay = '<div class="time-block">'
-    var textRow = '<div id="ppf' + counter + '" ><textarea class="row col-10" id="id' + counter + '"></textarea>'
-    var btnEl = '<button id=btnSave' + counter + ' ><div class="col-1 btn saveBtn"><i class="fas fa-save"></i></button></div></div></div>'
-    timeDisplay += timeRows.time + textRow + btnEl
+    var timeDisplay = '<div class="hour col"></div>'
     var getStorage = JSON.parse(localStorage.getItem("btnSave" + counter));
+    var textRow = '<textarea id=id' + counter + ' <div class="col-10" ></textarea>'
+    var btnEl = '<button id=btnSave' + counter + ' <div class="btn saveBtn col-1"><i class="fas fa-save"></i></button></div></div></div>'
+    timeDisplay += timeRows.time + textRow + btnEl
     $(".container").append(timeDisplay);
-    var parent = document.getElementById("ppf" + counter);
     if (getStorage) {
-        parent.querySelector("#" + "id" + counter).value = getStorage
+        document.getElementById("id" + counter).value = getStorage
     }
-    if (timeRows.time < timeEl) {
-        document.getElementById("ppf" + counter).className += "past";
-    } else if (timeRows.time == timeEl) {
-        document.getElementById("ppf" + counter).className += "present";
-    } else {
-        document.getElementById("ppf" + counter).className+= "future";
-    }
-    // console.log(localStorage)
     counter++
 });
 
-console.log($("div"))
-// Saves the local storage
+
 $(document).ready(function () {
     $("#btnSave0, #btnSave1, #btnSave2, #btnSave3, #btnSave4, #btnSave5, #btnSave6, #btnSave7, #btnSave8").on("click", function () {
-        var task = $("#ppf").children("textarea").val();
-        localStorage.setItem($(this).attr("#" + "id" + counter), JSON.stringify(task));
-        console.log($("#ppf").children("textarea"))
+        var task = $(this).prev().val();
+        localStorage.setItem($(this).attr('id'), JSON.stringify(task));
         console.log(task);
     }
     )
 });
+
+
+for (var i = 0; i < blockArray.length; i++) {
+
+    blockArray[i].textContent = localStorage.getItem(i);
+
+    if ((timeBlockEl[i].time) < hourNow) {
+        blockArray[i].classList.add("past");
+    } else if ((timeBlockEl[i].time) == hourNow) {
+        blockArray[i].classList.add("present");
+    } else if ((timeBlockEl[i].time) > hourNow) {
+        blockArray[i].classList.add("future");
+    }
+}
+console.log(timeBlockEl)
